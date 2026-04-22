d4c906933512f082efa45b54139086b58aee712e
Ondrej Locker <283214@vutbr.cz>
2026-04-22 09:03:57 +0200

    selection_sort

--- sorting.py
+++ sorting.py
@@ -3,8 +3,26 @@ import random
 
 def random_numbers(count, low=0, high=100):
     return [random.randint(low, high) for _ in range(count)]
-if __name__=="__main__":
-    values = random_numbers(10)  # 10 čísel v rozsahu 0–100
+
+def selection_sort(values):
+    n = 0
+    while n < len(values):
+        min_index = n
+        min_value = values[min_index]
+        for x in range(n+1, len(values)):
+            if values[x] < min_value:
+                min_index = x
+                min_value = values[x]
+        print(values)
+        values[n], values[min_index] = values[min_index], values[n]
+        n+=1
     print(values)
+
+
+
+if __name__=="__main__":
+    values = random_numbers(10)
+    # print(values)
     small = random_numbers(5, low=0, high=20)
-    print(small)
+    # print(small)
+    selection_sort(values)
