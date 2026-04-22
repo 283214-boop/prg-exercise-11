afdaf90937b90856247028f61f597b246030408f
Ondrej Locker <283214@vutbr.cz>
2026-04-22 10:08:00 +0200

    bublifuk

--- sorting.py
+++ sorting.py
@@ -1,5 +1,5 @@
 import random
-
+import matplotlib.pyplot as plt
 
 def random_numbers(count, low=0, high=100):
     return [random.randint(low, high) for _ in range(count)]
@@ -19,13 +19,30 @@ def selection_sort(values):
     return values
 
 def bubble_sort(values):
+    plt.ion()
+    plt.show()
     for j in range(len(values)):
+        swapped = False
 
         for n in range(len(values)-1):
 
             if values[n] > values[n+1]:
                 values[n], values[n+1] = values[n+1], values[n]
-        print(values)
+                swapped=True
+            if swapped == False:
+                break
+            index_highlight1 = n
+            index_highlight2 = n + 1
+            colors = ["steelblue"] * len(values)
+            colors[index_highlight1] = "tomato"
+            colors[index_highlight2] = "tomato"
+            plt.clf()
+            plt.bar(range(len(values)), values, color=colors)
+            plt.title("Bubble Sort")
+            plt.pause(0.1)
+    plt.ioff()
+    plt.show()
+    return values
 
 
 
@@ -33,7 +50,7 @@ def bubble_sort(values):
 
 
 if __name__=="__main__":
-    values = random_numbers(10)
+    values = random_numbers(100)
     print(values)
     small = random_numbers(5, low=0, high=20)
     # print(small)
